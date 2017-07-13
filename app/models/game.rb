f67d7bbbf4e5ecb4d1c5d4b5943e1dc90952575c
Alan Unruh <alan.unruh@gmail.com>
2017-07-13 21:50:35 +0000

    add scope and game tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,23 @@
 class Game < ApplicationRecord
-  belongs_to :white_player, class_name: 'User'
-  belongs_to :black_player, class_name: 'User'
+  belongs_to :white_player, class_name: 'User', optional: true
+  belongs_to :black_player, class_name: 'User', optional: true
 
   has_many :pieces
   has_many :moves
 
-  validates :white_player, presence: true
-  validates :black_player, presence: true
+  validates :white_player, presence: true, allow_blank: true
+  validates :black_player, presence: true, allow_blank: true
   validates :game_status, presence: true
+
+  # scope :avail, -> {where(avail: true)}
+
+  def avail
+    if white_player.present? == false
+      return true
+    elsif black_player.present? == false
+      return true
+    else
+      return false
+      end 
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Game, "scope" do
+    it "Show games with white and black player present as closed" do
+      # user1 = FactoryGirl.create(:white_player)
+      # user2 = FactoryGirl.create(:black_player) 
+      # post :create, { params: { white_player: user1.id, black_player: user2.id, game_status: "In play"}}
+      # FactoryGirl.create(:game)
+      game = FactoryGirl.create(:game, :full) 
+     
+
+      expect(game.avail).to eq(false)
+      # game = FactoryGirl.create(:game)
+
+    end
+
+    it "Shows games with white player present but no black player as open" do
+      game = FactoryGirl.create(:game, :no_bl)
+      expect(game.avail).to eq(true)
+    end
+
+    it "Shows games with white player not present but back player present as open" do
+      game = FactoryGirl.create(:game, :no_wh)
+      expect(game.avail).to eq(true)
+    end
+
+    it "Shows games with no white or black player present as open" do
+      game = FactoryGirl.create(:game, :no_players) 
+     
+
+      expect(game.avail).to eq(true)    
+    end
+
+  end
 end
